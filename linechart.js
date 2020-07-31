baab142dc519442c14b400e0d3d45e9382a7a5ae
Christophe Saillard <christophe.saillard@workday.com>
2020-07-31 10:02:19 +0200

    no-cache

--- linechart.js
+++ linechart.js
@@ -37,7 +37,7 @@ const selectdevices = (o) => {
 
 const refreshDevices = (id, callback) => {
     xhttp = new XMLHttpRequest();
-    xhttp.setRequestHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    xhttp.setRequestHeader("Cache-Control", "no-cache");
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             const options = JSON.parse(this.responseText)
