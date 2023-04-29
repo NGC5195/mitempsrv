2bdea16a5d9ef881b14911302a84f23e14c9b7ea
christophe.saillard <christophe.saillard@workayinternal.com>
2023-04-29 19:45:03 +0200

    another spinner

--- linechart.js
+++ linechart.js
@@ -1,5 +1,3 @@
-import {Spinner} from 'spin.js';
-
 const ctx = document.getElementById('myChart').getContext('2d')
 const monthLabel = ['Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Jui', 'Jui', 'Aoû', 'Sep', 'Oct', 'Noc', 'Déc']
 
@@ -135,6 +133,7 @@ const myTable = new Tabulator("#summary", {
 
 const loadData = (depth, forecast, device) => {
     var xhr = new XMLHttpRequest()
+    Spinner.show()
     xhr.open("GET", `./data?depth=${depth}&forecast=${forecast}&device=${device}`)
     xhr.onreadystatechange = () => { 
         if (xhr.readyState === 4 && xhr.status === 200) {
@@ -143,7 +142,8 @@ const loadData = (depth, forecast, device) => {
             myChart.update()
             myTable.setData(jsondata.summary)
             myTable.refresh
-            document.getElementById('timestamp').innerHTML = 'Dernière mesure à '+jsondata.timestamp            
+            document.getElementById('timestamp').innerHTML = 'Dernière mesure à '+jsondata.timestamp
+            Spinner.hide()
         }
     }
     xhr.send(null)
@@ -183,30 +183,9 @@ refreshDevices('devices', ()=> {
     document.getElementById('devices-select').value = device
 })
 
-var opts = {
-    lines: 13, // The number of lines to draw
-    length: 38, // The length of each line
-    width: 17, // The line thickness
-    radius: 45, // The radius of the inner circle
-    scale: 1, // Scales overall size of the spinner
-    corners: 1, // Corner roundness (0..1)
-    speed: 1, // Rounds per second
-    rotate: 0, // The rotation offset
-    animation: 'spinner-line-fade-quick', // The CSS animation name for the lines
-    direction: 1, // 1: clockwise, -1: counterclockwise
-    color: '#ffffff', // CSS color or array of colors
-    fadeColor: 'transparent', // CSS color or array of colors
-    top: '50%', // Top position relative to parent
-    left: '50%', // Left position relative to parent
-    shadow: '0 0 1px transparent', // Box-shadow for the lines
-    zIndex: 2000000000, // The z-index (defaults to 2e9)
-    className: 'spinner', // The CSS class to assign to the spinner
-    position: 'absolute', // Element positioning
-  };
-  
-var spinner = new Spinner(opts).spin(document.getElementById('spinner'));
+
 document.getElementById('period-select').value = depth
 document.getElementById('forecast-select').value = forecast
-document.getElementById('spinner').appendChild(spinner.el);
+Spinner();
 loadData(depth, forecast, device)
 refresh()
